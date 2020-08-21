611f63e57e46976be75fc81b197041ab2e799acc
Aaron Anderson <aaronlynanderson@gmail.com>
2020-08-21 15:06:04 -0500

    configure migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const UniswapV2Factory = artifacts.require("UniswapV2Factory");
 
 module.exports = function(deployer) {
-  deployer.deploy(UniswapV2Factory);
+  deployer.deploy(UniswapV2Factory, "0xEbB086500e0D6bF75F02Bc74bc69366F510100c6");
 };
